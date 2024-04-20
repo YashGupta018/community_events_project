6c66a236da5793e12f04f2ad2dcaba441cb698f4
YashGupta018 <ygupta2001@gmail.com>
2024-04-20 13:40:59 +0530

    in progress

---
+++ events/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from mapbox_location_field.admin import MapAdmin
+
+from .models import (
+    EventCategory,
+    Event,
+    JobCategory,
+    EventJobCategoryLinking,
+    EventMember,
+    EventUserWishList,
+    UserCoin,
+)
+
+# Register your models here.
+
+admin.site.register(EventCategory)
+admin.site.register(Event, MapAdmin)
+admin.site.register(JobCategory)
+admin.site.register(EventJobCategoryLinking)
+admin.site.register(EventMember)
+admin.site.register(EventUserWishList)
+admin.site.register(UserCoin)
